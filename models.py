d39083f303fee3cc77f139ce5192c2009a509bf4
gabrielcoelhoFJ7 <microwaycs1@gmail.com>
2025-08-29 14:09:10 -0300

    Venda > models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Float
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
 # Configuração do banco de dados
@@ -13,7 +13,7 @@ class Lanche(Base):
     id_lanche = Column(Integer, primary_key=True)
     nome_lanche = Column(String(20), nullable=False, index=True)
     descricao_lanche = Column(String(255), index=True)
-    disponivel = Column(Boolean, default=False, index=True)
+    disponivel = Column(Boolean, default=True, index=True)
 
     def __repr__(self):
         return '<Lanche: {} {}>'.format(self.id_lanche, self.nome_lanche)
@@ -77,6 +77,42 @@ class Insumo(Base):
             'validade': self.validade,
         }
         return var_insumo
+
+class Venda(Base):
+    __tablename__ = 'vendas'
+    id_venda = Column(Integer, primary_key=True)
+    data_venda = Column(String(10), nullable=False, index=True)
+    valor_venda = Column(Float, nullable=False, index=True)
+    status_concluida = Column(Boolean, default=True, index=True)
+
+    def __repr__(self):
+        return '<Venda: {} {}>'.format(self.id_venda, self.data_venda)
+
+    def save(self, db_session):
+        try:
+            db_session.add(self)
+            db_session.commit()
+        except:
+            db_session.rollback()
+            raise
+
+    def delete(self, db_session):
+        try:
+            db_session.delete(self)
+            db_session.commit()
+        except:
+            db_session.rollback()
+            raise
+
+    def serialize(self):
+        var_venda = {
+            'id_venda': self.id_venda,
+            'data_venda': self.data_venda,
+            'valor_venda': self.valor_venda,
+            'status_concluida': self.status_concluida,
+        }
+        return var_venda
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
